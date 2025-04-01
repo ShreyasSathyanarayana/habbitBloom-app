104ce049f7662943c9f859f07c3bab67d33f5ea5
ShreyasSathyanarayana <shreyas24s2001@gmail.com>
2025-04-01 20:58:35 +0530

    impored perfomance in TabBar

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -13,7 +13,9 @@ import { BlurView } from "expo-blur";
 import { verticalScale } from "@/metric";
 import { useTabBar } from "@/context/TabBarContext";
 import Animated, {
+  LayoutAnimationConfig,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withSpring,
   withTiming,
@@ -25,13 +27,14 @@ const TabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   const greyColor = "rgba(157, 178, 206, 1)";
   const { isTabBarVisible } = useTabBar();
   const { bottom: paddingBottom } = useSafeAreaInsets();
-  const translateY = useSharedValue(0);
-  useEffect(() => {
-    translateY.value = withSpring(isTabBarVisible ? 0 : 100, {
-      damping: 40,
-      stiffness: 200,
-    });
-  }, [isTabBarVisible]);
+  // const translateY = useSharedValue(0);
+  const translateY = useDerivedValue(() =>
+    withSpring(isTabBarVisible ? 0 : 100, {
+      damping: 40, // Less damping for a snappier effect
+      stiffness: 200, // Balanced stiffness for quick response
+      // mass: 0.5, // Lighter animation for smoother movement
+    })
+  );
 
   // Animated style for tab bar
   const animatedStyle = useAnimatedStyle(() => ({
@@ -125,94 +128,96 @@ const TabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   //   );
   // }
   return (
-    <Animated.View style={Platform.OS !== "ios" && animatedStyle}>
-      {/** this is only for android */}
-      <View
-        style={[
-          styles.tabbar,
-          {
-            paddingBottom:
-              Platform.OS === "ios"
-                ? verticalScale(paddingBottom)
-                : verticalScale(10),
-            borderTopWidth: 0.5,
-            borderColor: "rgba(138, 43, 226, 1)",
-          },
-          { backgroundColor: "black" },
-        ]}
-      >
-        {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
+    <LayoutAnimationConfig skipEntering>
+      <Animated.View style={Platform.OS !== "ios" && animatedStyle}>
+        {/** this is only for android */}
+        <View
+          style={[
+            styles.tabbar,
+            {
+              paddingBottom:
+                Platform.OS === "ios"
+                  ? verticalScale(paddingBottom)
+                  : verticalScale(10),
+              borderTopWidth: 0.5,
+              borderColor: "rgba(138, 43, 226, 1)",
+            },
+            { backgroundColor: "black" },
+          ]}
+        >
+          {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.name;
 
-          if (["_sitemap", "+not-found"].includes(route.name)) return null;
+            if (["_sitemap", "+not-found"].includes(route.name)) return null;
 
-          const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
+            const onPress = () => {
+              const event = navigation.emit({
+                type: "tabPress",
+                target: route.key,
+                canPreventDefault: true,
+              });
 
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
-            }
-          };
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name, route.params);
+              }
+            };
 
-          const onLongPress = () => {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key,
-            });
-          };
+            const onLongPress = () => {
+              navigation.emit({
+                type: "tabLongPress",
+                target: route.key,
+              });
+            };
 
-          return (
-            <TabBarButton
-              key={route.name}
-              // style={styles.tabbarItem}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              isFocused={isFocused}
-              routeName={route.name}
-              color={isFocused ? primaryColor : greyColor}
-              label={label}
-            />
-          );
+            return (
+              <TabBarButton
+                key={route.name}
+                // style={styles.tabbarItem}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                isFocused={isFocused}
+                routeName={route.name}
+                color={isFocused ? primaryColor : greyColor}
+                label={label}
+              />
+            );
 
-          // return (
-          //   <TouchableOpacity
-          //     key={route.name}
-          //     style={styles.tabbarItem}
-          //     accessibilityRole="button"
-          //     accessibilityState={isFocused ? { selected: true } : {}}
-          //     accessibilityLabel={options.tabBarAccessibilityLabel}
-          //     testID={options.tabBarTestID}
-          //     onPress={onPress}
-          //     onLongPress={onLongPress}
-          //   >
-          //     {
-          //         icons[route.name]({
-          //             color: isFocused? primaryColor: greyColor
-          //         })
-          //     }
-          //     <Text style={{
-          //         color: isFocused ? primaryColor : greyColor,
-          //         fontSize: 11
-          //     }}>
-          //       {label}
-          //     </Text>
-          //   </TouchableOpacity>
-          // );
-        })}
-      </View>
-    </Animated.View>
+            // return (
+            //   <TouchableOpacity
+            //     key={route.name}
+            //     style={styles.tabbarItem}
+            //     accessibilityRole="button"
+            //     accessibilityState={isFocused ? { selected: true } : {}}
+            //     accessibilityLabel={options.tabBarAccessibilityLabel}
+            //     testID={options.tabBarTestID}
+            //     onPress={onPress}
+            //     onLongPress={onLongPress}
+            //   >
+            //     {
+            //         icons[route.name]({
+            //             color: isFocused? primaryColor: greyColor
+            //         })
+            //     }
+            //     <Text style={{
+            //         color: isFocused ? primaryColor : greyColor,
+            //         fontSize: 11
+            //     }}>
+            //       {label}
+            //     </Text>
+            //   </TouchableOpacity>
+            // );
+          })}
+        </View>
+      </Animated.View>
+    </LayoutAnimationConfig>
   );
 };
 
